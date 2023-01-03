1d270b789c6799b8aa00df0cfb5ad1dfe31d2dc9
Bibek Rawat <bibekrawat14@gmail.com>
2023-01-03 13:31:36 +0545

    String challenge solution created

--- Program.cs
+++ Program.cs
@@ -5,5 +5,31 @@ string output = "";
 
 // Your work here
 
-Console.WriteLine(quantity);
-Console.WriteLine(output);
+const string spanTag = "<span>";
+
+// Extract the quantity
+int quantityStart = input.IndexOf(spanTag);
+int quantityEnd = input.IndexOf("</span>");
+quantityStart += spanTag.Length;
+int quantityLength = quantityEnd - quantityStart;
+quantity = input.Substring(quantityStart, quantityLength);
+
+// Set output to input, replacing the trademark symbol with the registered trademark symbol
+output = input.Replace("&trade;", "&reg;");
+
+// Remove the opening <div> tag
+int divStart = input.IndexOf("<div");
+int divEnd = input.IndexOf(">");
+int divLength = divEnd - divStart;
+divLength += 1;
+output = output.Remove(divStart, divLength);
+
+// Remove the closing <div> tag
+int divCloseStart = output.IndexOf("</div");
+int divCloseEnd = output.IndexOf(">", divCloseStart);
+int divCloseLength = divCloseEnd - divCloseStart;
+divCloseLength += 1;
+output = output.Remove(divCloseStart, divCloseLength);
+
+Console.WriteLine($"Quantity: {quantity}");
+Console.WriteLine($"Output: {output}");
