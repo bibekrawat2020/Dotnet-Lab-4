2b190e1731b0fff76e1134cad2825995814c3dd7
Bibek Rawat <bibekrawat14@gmail.com>
2023-01-03 10:40:23 +0545

    Updated code to retrieve value from one or more sets of paranthesis

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
-﻿string message = "(What if) I am (only interested) in the last (set of parentheses)?";
-int openingPosition = message.LastIndexOf('(');
+﻿string message = "(What if) there are (more than) one (set of parentheses)?";
+while (true)
+{
+    int openingPosition = message.IndexOf('(');
+    if (openingPosition == -1) break;
+    openingPosition += 1;
+    int closingPosition = message.IndexOf(')');
+    // Console.WriteLine(closingPosition);
+    int length = closingPosition - openingPosition;
+    Console.WriteLine(message.Substring(openingPosition, length));
 
-openingPosition += 1;
-int closingPosition = message.LastIndexOf(')');
-int length = closingPosition - openingPosition;
-Console.WriteLine(message.Substring(openingPosition, length));
+    // Note how we use the overload of Substring to return only the remaining 
+    // unprocessed message:
+    message = message.Substring(closingPosition + 1);
+}
