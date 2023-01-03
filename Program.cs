ab4339d3f678c2423470aab9ce2a0186effd23b8
Bibek Rawat <bibekrawat14@gmail.com>
2023-01-03 13:29:46 +0545

    String challenge question

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿string message = "This--is--ex-amp-le--da-ta";
-message = message.Replace("--", " ");
-message = message.Replace("-", "");
-Console.WriteLine(message);
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
